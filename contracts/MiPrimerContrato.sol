11169632857a497c0e0c7bd9c46ba3f480b6bd39
Camila Vildoso <156922629+CamilaVildoso@users.noreply.github.com>
2024-02-20 17:48:31 -0300

    Update MiPrimerContrato.sol

--- contracts/MiPrimerContrato.sol
+++ contracts/MiPrimerContrato.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.21;
 
 contract MiPrimerContrato {
     string saludo;
-    string nombreYApellido = "Lee Marreros";
+    string nombreYApellido = "Camila Vildoso";
 
     function set(string memory _nuevoSaludo) public {
         saludo = _nuevoSaludo; // no se necesita 'this'
